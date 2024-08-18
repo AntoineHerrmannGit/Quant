e4e25e53268836f5858fbdab0c4db16e0db4da17
Antoine Herrmamm <117520250+AntoineHerrmannGit@users.noreply.github.com>
2024-08-18 20:15:46 +0200

    Create ReadMe.cmd

---
+++ Model/ReadMe.cmd
@@ -0,0 +1,7 @@
+# Models
+--------
+Contains all DataStructures used in the entire code. 
+Only constructors, accessors and operators overload must be declared in the models. 
+Associated features must be written in Extensions.
+
+Enums are treated apart due to their special nature.
