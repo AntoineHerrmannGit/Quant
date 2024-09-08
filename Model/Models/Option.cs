5cbf13e2fe9d62eaca3b5bbc476d85cf35c0abad
Antoine Herrmann <antoine67.herrmann@laposte.net>
2024-09-08 16:00:15 +0200

    [Models]
    Set option's instrument to nullable

--- Model/Models/Option.cs
+++ Model/Models/Option.cs
@@ -7,7 +7,7 @@ public class Option : Instrument
     public double Value { get; set; }
     public double Strike { get; set; }
     public DateTime Maturity { get; set; }
-    public Instrument Underlying { get; set; }
+    public Instrument? Underlying { get; set; }
     public OptionType Type { get; set; }
     public double Delta { get; set; }
     public double Gamma { get; set; }
