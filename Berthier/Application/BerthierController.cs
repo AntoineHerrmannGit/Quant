6bf5ea788854fad20b86b29d59a29ff59336513e
Antoine Herrmann <antoine67.herrmann@laposte.net>
2024-09-22 11:01:31 +0200

    [Berthier] Setup controller

--- Berthier/Application/BerthierController.cs
+++ Berthier/Application/BerthierController.cs
@@ -1,5 +1,7 @@
+using Berthier.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using YahooFinanceApi;
 
 namespace Berthier.Application
 {
@@ -7,5 +9,16 @@ namespace Berthier.Application
     [ApiController]
     public class BerthierController : ControllerBase
     {
+        private readonly IBerthierService _berthierService;
+        public BerthierController(IBerthierService berthierService)
+        {
+            _berthierService = berthierService;
+        }
+
+        [HttpGet(template: "spots", Name="GetSpots")]
+        public async Task<IDictionary<string, IEnumerable<Candle>>> GetSpots(List<string> tickers, DateTime startDate, DateTime endDate, string interval)
+        {
+            return await _berthierService.GetSpots(tickers, startDate, endDate, interval).ConfigureAwait(false);
+        }
     }
 }
--- Berthier/Berthier.cs
+++ Berthier/Berthier.cs
@@ -1,6 +1,50 @@
-﻿namespace Berthier;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-public class Berthier
+namespace Berthier;
+
+public class Program
 {
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
+        // Add services to the container.
+        ServiceRegistrator.ConfigureServices(builder.Services);
+
+        // Configurer Swagger pour la documentation API
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo 
+            { 
+                Title = "Berthier", 
+                Version = "v1" 
+            });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Berthier");
+                c.RoutePrefix = string.Empty; // Pour exposer Swagger à la racine
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers(); // Map les routes définies dans les controllers
+
+        app.Run();
+    }
 }
